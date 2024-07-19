4124570cc5bb75cb181c6fd44e3f5e69f0952beb
ZaynMeledath <zaynmeledath@gmail.com>
2024-07-19 12:49:00 +0530

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:turning_point_tasks_app/view/login/login_screen.dart';
 
 void main() {
